b47aaf06bdb56d1adc1a99824b6c3d9f503e69d6
WAFulbright <bill@2100solutions.com>
2019-05-07 21:25:44 -0400

    Initial Commit

---
+++ pod-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pod-test
+//
+//  Created by WilliamFulbright on 5/7/19.
+//  Copyright © 2019 WilliamFulbright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pod-testTests/pod_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  pod_testTests.swift
+//  pod-testTests
+//
+//  Created by WilliamFulbright on 5/7/19.
+//  Copyright © 2019 WilliamFulbright. All rights reserved.
+//
+
+import XCTest
+@testable import pod_test
+
+class pod_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
